5302e0ae8ee0227277ea12fb97ce3bf877753b8a
kachraz <kachraz@tuta.io>
2024-08-29 10:56:06 +0530

    👩‍🦰 3rd Try

--- src/comp/TabComponents/TabButton.jsx
+++ src/comp/TabComponents/TabButton.jsx
@@ -1,11 +1,11 @@
 // Buttons compoenent - note the onselet prop is a custom prop, any number of these ca be set
 
-export default function TabButton({ children, onSelect, isSelected }) {
+export default function TabButton({ children, isSelected, ...props }) {
   console.log("Tab Execute");
 
   return (
     <li>
-      <button className={isSelected ? "active" : ""} onClick={onSelect}>
+      <button className={isSelected ? "active" : ""} {...props}>
         {children}
       </button>
     </li>
