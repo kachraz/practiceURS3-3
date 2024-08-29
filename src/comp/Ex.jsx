46099e8f159c6ca1eaf73bf0881445a47ef1ff27
kachraz <kachraz@tuta.io>
2024-08-29 11:18:05 +0530

    👩‍🦰 3rd Try

--- src/comp/Ex.jsx
+++ src/comp/Ex.jsx
@@ -31,6 +31,7 @@ export default function Exa() {
   return (
     <Section title="Examples" id="examples">
       <Tabs
+        buttonsContainer="menu"
         buttons={
           <>
             <TabButton
